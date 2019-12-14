502397bd8b09aa14628e1fc5346ee65a3fb8ebf3
Younase <youigta@gmail.com>
2019-12-14 22:34:55 +0100

    working hidden correction, added error

--- net.c
+++ net.c
@@ -132,12 +132,13 @@ void print_err(float* err,int s){
 
 int train(net* network,int* in,int* out,int length,float le_rate){
   //flag holds last it where weights modded
-int max_it=1,curr_it,flag=0,i,j,k,l,max=max_tab(network->npl,network->num_layers);
-float *tmp,*err=malloc(max*sizeof(float)),*last_err=malloc(max*sizeof(float));
+int max_it=10000,curr_it,flag=0,i,j,k,l,max=max_tab(network->npl,network->num_layers);
+float error,**weights,*tmp,*err=malloc(max*sizeof(float)),*last_err=malloc(max*sizeof(float));
 curr_it=max_it;
 while(flag!=curr_it+length && curr_it){
   curr_it--;
   for (i = 0; i < length; i++) {
+    error=0;
     eval(network,in+i*network->npl[0]);//////evaluate network
     if(dbg){
       printf("desired output:\n" );
@@ -152,28 +153,86 @@ while(flag!=curr_it+length && curr_it){
     }
     flag=curr_it;
     printf("weights getting modded\n");
-
+//////     TESTED UNTIL HERE
+    ///allocating for old_weights
+    weights=(float**)malloc((1+network->npl[network->num_layers-2])*sizeof(float*));//bias not needed, but added for j loop compat
+    for(j=0;j<=network->npl[network->num_layers-2];j++){
+      weights[j]=(float*)malloc(network->npl[network->num_layers-1]*sizeof(float*));
+    }
     //////     OUTPUT ERROR
     for(k=1;k<=network->npl[network->num_layers-1];k++){
       err[k-1]=(out[i*network->npl[1]+k-1]-network->neuron[network->num_layers-1][k])
-              *network->act_der(network->neuron[network->num_layers-1][k]);
+              *network->act_der(network->neuron[network->num_layers-1][k]);   ///// no error for bias
+      error+=(out[i*network->npl[1]+k-1]-network->neuron[network->num_layers-1][k])*
+      (out[i*network->npl[1]+k-1]-network->neuron[network->num_layers-1][k]);
       ///     OUTPUT WEIGHT
-      for(j=0;j<=network->npl[network->num_layers-2];j++)
+
+      for(j=0;j<=network->npl[network->num_layers-2];j++){
+        weights[j][k-1]=network->weight[network->num_layers-2][j][k-1];
         network->weight[network->num_layers-2][j][k-1]+=le_rate*err[k-1]*network->neuron[network->num_layers-2][j];
-            }
+      }
+    }
+
 print_err(err,max);
-//////     TESTED UNTIL HERE
 tmp=last_err;
 last_err=err;
 err=tmp;
-//////     TESTED UNTIL HERE
+
 // print_err(err,max);
 // print_err(last_err,max);
 
+/**********   HIDDEN ERRORS    ***********/
+
+  for(l=network->num_layers-2;l>0;l--){
 
 
-  }
 
+
+      for(j=1;j<=network->npl[l];j++){
+        err[j-1]=0;
+        for(k=0;k<network->npl[l+1];k++){
+          err[j-1]+=weights[j][k]*last_err[k];//add -1
+        }
+        err[j-1]*=network->act_der(network->neuron[l][j]);
+        free(weights[j]);
+        // printf("%f\t",err[j-1]);
+        }
+        // for(j=0;j<=network->npl[l];j++){
+        //   free(weights[j]);
+        // }
+        free(weights);
+        weights=(float**)malloc((1+network->npl[l])*sizeof(float*));//bias not needed, but added for j loop compat
+        for(j=0;j<=network->npl[l];j++){
+          weights[j]=(float*)malloc(network->npl[l+1]*sizeof(float*));
+        }
+        ///// free/allocate done
+        /////save weights
+        /////mod weights
+        for(k=0;k<=network->npl[l+1];k++){
+          for(j=0;j<=network->npl[l];j++){
+            weights[j][k]=network->weight[l-1][j][k];
+            network->weight[l-1][j][k]+=le_rate*err[k]*network->neuron[l-1][j];
+          }
+        }
+        //
+        // for(k=1;k<=network->npl[l+1];k++){
+        //   for(j=0;j<=network->npl[l];j++){
+        //     weights[j][k-1]=network->weight[l][j][k-1];
+        //     network->weight[l][j][k-1]+=le_rate*err[k-1]*network->neuron[l][j];
+        //   }
+        // }
+
+        print_err(err,max);
+        tmp=last_err;
+        last_err=err;
+        err=tmp;
+    }
+
+
+
+}
+error=0.5*error;
+printf("error = %f///////////////////////////////////////\n",error );
 }
 return curr_it;
 }
