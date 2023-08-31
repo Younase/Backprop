6c5b6ab172dcc78f66426cd24adf32d4c6a9b42b
Younase <youigta@gmail.com>
2023-08-31 21:28:38 +0100

    perf: reduce d_hid allocation size

--- net.c
+++ net.c
@@ -35,7 +35,7 @@ network* net_init(int num_layers, int* nrns_per_layer, char* activation){
     net->neuron=(double**)malloc(num_layers*sizeof(double*));
     net->weight=(double***)malloc(num_layers*sizeof(double**));
     net->d_out=(double*)malloc(nrns_per_layer[ net->num_layers - 1 ]*sizeof(double));
-    net->d_hid=(double**)malloc(net->num_layers*sizeof(double*));
+    net->d_hid=(double**)malloc((net->num_layers-2)*sizeof(double*));
 
     /*INIT NEURONS*/
     for (int i=0; i<num_layers;i++){        /*for each layer*/
